765dd39fb4115a9d3553d5ab90c249fdb1f31442
Samuel <ajiboyeadebayo901@gmail.com>
2025-04-19 13:50:06 +0100

    modified user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,8 +6,11 @@ const Role = require('../models/role');
 exports.addUser = async (req, res) => {
   const { fullName, email, roleName } = req.body;
   try {
-    const role = await Role.findOne({ name: roleName });
-    if (!role) return res.status(404).json({ error: 'Role not found' });
+    let role = await Role.findOne({ name: roleName });
+
+    if (!role) {
+      role = await Role.create({ name: roleName, permissions: [] });
+    }
 
     const defaultPassword = 'password123';
     const hashedPassword = await bcrypt.hash(defaultPassword, 10);
